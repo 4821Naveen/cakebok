ff03756aa782565c5d84cdc1a22ba5f109e0a3b5
4821Naveen <97329974+4821Naveen@users.noreply.github.com>
2026-02-10 16:11:03 +0530

    Fix usePathname client component issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/uploads/:path*',
+        destination: 'http://localhost:3001/uploads/:path*', // Proxy to Admin App
+      },
+    ];
+  },
 };
 
 export default nextConfig;
